7f124399e794576527fd0147a5198e289c12757b
Gopala Krishnan Srinivasan <46772742+gopalakrishnan94@users.noreply.github.com>
2024-09-23 03:12:39 +0530

    Create Villa

--- Villa_VillaAPI/Controllers/VillaAPIController.cs
+++ Villa_VillaAPI/Controllers/VillaAPIController.cs
@@ -10,14 +10,35 @@ namespace Villa_VillaAPI.Controllers;
 public class VillaAPIController : ControllerBase
 {
     [HttpGet]
-    public IEnumerable<VillaDTO> GetVillas() 
+    public ActionResult<IEnumerable<VillaDTO>> GetVillas() 
     {
-        return VillaStore.villaList;
+        return Ok(VillaStore.villaList);
     }
 
     [HttpGet("{id:int}")]
-    public VillaDTO GetVilla(int id) 
+    public ActionResult<VillaDTO> GetVilla(int id) 
     {
-        return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+        if (id == 0) {
+            return BadRequest();
+        }
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        if (villa == null) {
+            return NotFound();
+        }
+        return Ok(villa);
+    }
+    
+    [HttpPost]
+    public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO) 
+    {
+        if (villaDTO == null) {
+            return BadRequest();
+        }
+        if (villaDTO.Id > 0) {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+        villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+        VillaStore.villaList.Add(villaDTO);
+        return Ok(villaDTO);
     }
 }
