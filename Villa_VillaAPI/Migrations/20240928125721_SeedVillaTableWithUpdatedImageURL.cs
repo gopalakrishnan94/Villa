005524f9c88557f3e37e9275c3fa1a5e48e18b1f
Gopala Krishnan Srinivasan <46772742+gopalakrishnan94@users.noreply.github.com>
2024-09-28 18:29:26 +0530

    Seed Villa Table With Updated Image URL

---
+++ Villa_VillaAPI/Migrations/20240928125721_SeedVillaTableWithUpdatedImageURL.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Villa_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaTableWithUpdatedImageURL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 27, 21, 505, DateTimeKind.Local).AddTicks(610), "https://raw.githubusercontent.com/gopalakrishnan94/Villa/refs/heads/main/Images/villa_01.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 27, 21, 505, DateTimeKind.Local).AddTicks(630), "https://raw.githubusercontent.com/gopalakrishnan94/Villa/refs/heads/main/Images/villa_02.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 27, 21, 505, DateTimeKind.Local).AddTicks(640), "https://raw.githubusercontent.com/gopalakrishnan94/Villa/refs/heads/main/Images/villa_03.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 27, 21, 505, DateTimeKind.Local).AddTicks(640), "https://raw.githubusercontent.com/gopalakrishnan94/Villa/refs/heads/main/Images/villa_04.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 27, 21, 505, DateTimeKind.Local).AddTicks(640), "https://raw.githubusercontent.com/gopalakrishnan94/Villa/refs/heads/main/Images/villa_05.jpg" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 18, 13, 922, DateTimeKind.Local).AddTicks(9240), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 18, 13, 922, DateTimeKind.Local).AddTicks(9320), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 18, 13, 922, DateTimeKind.Local).AddTicks(9320), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 18, 13, 922, DateTimeKind.Local).AddTicks(9320), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2024, 9, 28, 18, 18, 13, 922, DateTimeKind.Local).AddTicks(9330), "" });
+        }
+    }
+}
