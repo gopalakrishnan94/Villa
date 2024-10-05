bc28e0d4a0609582035cdee3481fe76b0b8a09df
Gopala Krishnan Srinivasan <46772742+gopalakrishnan94@users.noreply.github.com>
2024-10-05 19:05:57 +0530

    Villa_VillaWEB Project Created

---
+++ Villa_VillaWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Villa_VillaWEB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
